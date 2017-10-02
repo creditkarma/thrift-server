901d66652862eec73003d122606ab6432d5eb303
Kevin Greene <kevin.greene@creditkarma.com>
2017-10-02 08:52:01 -0700

    Update sample app server to use hapi thrift plugin

--- packages/thrift-client/example/server.ts
+++ packages/thrift-client/example/server.ts
@@ -4,42 +4,65 @@ import {
   TBufferedTransport,
 } from 'thrift'
 
+import { ThriftPlugin } from '@creditkarma/thrift-server-hapi'
+import Hapi = require('hapi');
+
 import { Operation, Calculator, Work } from './codegen/tutorial/tutorial'
+import { SharedStruct } from './codegen/shared/shared'
 
-const myServiceHandler = {
-  ping(): void {},
-  add(a: number, b: number): number {
-    return a + b;
-  },
-  calculate(logId: number, work: Work): number {
-    switch (work.op) {
-      case Operation.ADD:
-        return work.num1 + work.num2;
-      case Operation.SUBTRACT:
-        return work.num1 - work.num2;
-      case Operation.DIVIDE:
-        return work.num1 / work.num2;
-      case Operation.MULTIPLY:
-        return work.num1 * work.num2;
+const server: Hapi.Server = new Hapi.Server();
+server.connection({ port: 8045, host: 'localhost' });
+
+server.register(ThriftPlugin, (err: any) => {
+  if (err) {
+    throw err;
+  }
+});
+
+const impl = new Calculator.Processor({
+    ping(): void {},
+    add(a: number, b: number): number {
+      return a + b;
+    },
+    calculate(logId: number, work: Work): number {
+      switch (work.op) {
+        case Operation.ADD:
+          return work.num1 + work.num2;
+        case Operation.SUBTRACT:
+          return work.num1 - work.num2;
+        case Operation.DIVIDE:
+          return work.num1 / work.num2;
+        case Operation.MULTIPLY:
+          return work.num1 * work.num2;
+      }
+    },
+    zip(): void {},
+    getStruct(): SharedStruct {
+      return new SharedStruct();
     }
-  },
-  zip(): void {},
-};
+})
 
-const myServiceOpts = {
-  handler: myServiceHandler,
-  processor: Calculator,
-  protocol: TBinaryProtocol,
-  transport: TBufferedTransport
-};
+server.route({
+    method: 'POST',
+    path: '/',
+    handler: {
+        thrift: {
+            service: impl,
+        },
+    },
+    config: {
+        payload: {
+            parse: false,
+        },
+    },
+});
 
-const serverOpt = {
-   services: {
-      '/': myServiceOpts
-   }
-};
+server.start((err: any) => {
+  if (err) {
+    throw err;
+  }
 
-const port: number = 8045;
-createWebServer<Calculator.Processor<void>, Calculator.IHandler<void>>(serverOpt).listen(port, () => {
-  console.log(`Thrift server listening on port ${port}`)
+  if (server.info !== null) {
+    console.log(`Thrift server running at: ${server.info.uri}`);
+  }
 });
