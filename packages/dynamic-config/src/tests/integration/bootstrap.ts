0cfb3fe48071b5fccb1c573ef4cdfae47d816f48
Kevin Greene <kevin.greene@creditkarma.com>
2017-12-11 20:12:46 -0800

    Add timeout before test bootstrap

--- packages/dynamic-config/src/tests/integration/bootstrap.ts
+++ packages/dynamic-config/src/tests/integration/bootstrap.ts
@@ -9,74 +9,76 @@ import * as path from 'path'
 
 process.chdir(__dirname)
 
-const consulClient: KvStore = new KvStore('http://localhost:8510')
-const vaultClient: VaultClient = new VaultClient({
-  apiVersion: 'v1',
-  destination: 'http://localhost:8210',
-  namespace: 'secret',
-  tokenPath: './tmp/token',
-})
-
-const token: string = execSync('curl http://localhost:8211/client-token').toString()
-
-function rootDir(): string {
-  if (os.platform() === 'win32') {
-    return process.cwd().split(path.sep)[0]
-  } else {
-    return '/'
-  }
-}
-
-function createPath(parts: Array<string>, soFar: string): void {
-  const current: string = path.join(soFar, parts[0])
-  if (!fs.existsSync(current)) {
-    fs.mkdirSync(current)
-  }
-
-  if (parts.length > 1) {
-    createPath(parts.slice(1), current)
-  }
-}
-
-function mkdir(dirPath: string): void {
-  const parts: Array<string> = dirPath.split(path.sep).filter((val: string) => val !== '')
-
-  // Check for absolute path
-  if (parts.length > 0 && path.isAbsolute(dirPath)) {
-    createPath(parts, rootDir())
-  } else if (parts.length > 0) {
-    createPath(parts, process.cwd())
-  }
-}
-
-// create directory for test token
-mkdir('./tmp')
-
-fs.writeFile('./tmp/token', token, (err: any) => {
-  Promise.all([
-    consulClient.set({ path: 'test-config-one' }, {
-      database: {
-        username: 'testUser',
-        password: 'testPass',
-      },
-    }),
-    consulClient.set({ path: 'test-config-two' }, {
-      database: {
-        username: 'fakeUser',
-      },
-    }),
-    consulClient.set({ path: 'with-vault' }, {
-      'hashicorp-vault': {
-        apiVersion: 'v1',
-        destination: 'http://localhost:8210',
-        namespace: 'secret',
-        tokenPath: './tmp/token',
-      },
-    }),
-    vaultClient.set('test-secret', 'this is a secret'),
-  ]).then((result: any) => {
-    console.log('Done populating mock data')
-  }, (failure: any) => {
-    console.log('Error populating mock data: ', failure)
+setTimeout(() => {
+  const consulClient: KvStore = new KvStore('http://localhost:8510')
+  const vaultClient: VaultClient = new VaultClient({
+    apiVersion: 'v1',
+    destination: 'http://localhost:8210',
+    namespace: 'secret',
+    tokenPath: './tmp/token',
   })
-})
+
+  const token: string = execSync('curl http://localhost:8211/client-token').toString()
+
+  function rootDir(): string {
+    if (os.platform() === 'win32') {
+      return process.cwd().split(path.sep)[0]
+    } else {
+      return '/'
+    }
+  }
+
+  function createPath(parts: Array<string>, soFar: string): void {
+    const current: string = path.join(soFar, parts[0])
+    if (!fs.existsSync(current)) {
+      fs.mkdirSync(current)
+    }
+
+    if (parts.length > 1) {
+      createPath(parts.slice(1), current)
+    }
+  }
+
+  function mkdir(dirPath: string): void {
+    const parts: Array<string> = dirPath.split(path.sep).filter((val: string) => val !== '')
+
+    // Check for absolute path
+    if (parts.length > 0 && path.isAbsolute(dirPath)) {
+      createPath(parts, rootDir())
+    } else if (parts.length > 0) {
+      createPath(parts, process.cwd())
+    }
+  }
+
+  // create directory for test token
+  mkdir('./tmp')
+
+  fs.writeFile('./tmp/token', token, (err: any) => {
+    Promise.all([
+      consulClient.set({ path: 'test-config-one' }, {
+        database: {
+          username: 'testUser',
+          password: 'testPass',
+        },
+      }),
+      consulClient.set({ path: 'test-config-two' }, {
+        database: {
+          username: 'fakeUser',
+        },
+      }),
+      consulClient.set({ path: 'with-vault' }, {
+        'hashicorp-vault': {
+          apiVersion: 'v1',
+          destination: 'http://localhost:8210',
+          namespace: 'secret',
+          tokenPath: './tmp/token',
+        },
+      }),
+      vaultClient.set('test-secret', 'this is a secret'),
+    ]).then((result: any) => {
+      console.log('Done populating mock data')
+    }, (failure: any) => {
+      console.log('Error populating mock data: ', failure)
+    })
+  })
+}, 2000)
