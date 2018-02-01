b5c97dd6e4f981b834d54a94b9424c0c7c638567
Kevin Greene <kevin.greene@creditkarma.com>
2018-02-01 13:05:40 -0800

    Fix test thrift files

--- packages/thrift-server-express/src/tests/thrift/calculator.thrift
+++ packages/thrift-server-express/src/tests/thrift/calculator.thrift
@@ -105,9 +105,9 @@ enum Operation {
  * manual management in some languages.
  */
 struct Work {
-  1: i32 num1 = 0,
-  2: i32 num2,
-  3: Operation op,
+  1: required i32 num1 = 0,
+  2: required i32 num2,
+  3: required Operation op,
   4: optional string comment,
 }
 
