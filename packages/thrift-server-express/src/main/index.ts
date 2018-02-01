f0c69e4d319fc83a4ac6ef6ffb670bc57ab245e4
Kevin Greene <kevin.greene@creditkarma.com>
2018-02-01 13:12:37 -0800

    Fix tests for express

--- packages/thrift-server-express/src/main/index.ts
+++ packages/thrift-server-express/src/main/index.ts
@@ -1,41 +1,41 @@
 import {
-  getProtocol,
-  getService,
-  getTransport,
-  IProcessorConstructor,
-  IProtocolConstructor,
-  IThriftProcessor,
-  ITransportConstructor,
-  process,
-  ProtocolType,
-  TransportType,
+    getProtocol,
+    getService,
+    getTransport,
+    IProcessorConstructor,
+    IProtocolConstructor,
+    IThriftProcessor,
+    ITransportConstructor,
+    process,
+    ProtocolType,
+    TransportType,
 } from '@creditkarma/thrift-server-core'
 
 import * as express from 'express'
 
 export interface IPluginOptions {
-  transport?: TransportType
-  protocol?: ProtocolType
+    transport?: TransportType
+    protocol?: ProtocolType
 }
 
 export function thriftExpress<TProcessor extends IThriftProcessor<express.Request>, THandler>(
-  Service: IProcessorConstructor<TProcessor, THandler>,
-  handlers: THandler,
-  options: IPluginOptions = {}): express.RequestHandler {
+    Service: IProcessorConstructor<TProcessor, THandler>,
+    handlers: THandler,
+    options: IPluginOptions = {}): express.RequestHandler {
 
-  const Transport: ITransportConstructor = getTransport(options.transport)
-  const Protocol: IProtocolConstructor = getProtocol(options.protocol)
-  const service: TProcessor = getService(Service, handlers)
+    const Transport: ITransportConstructor = getTransport(options.transport)
+    const Protocol: IProtocolConstructor = getProtocol(options.protocol)
+    const service: TProcessor = getService(Service, handlers)
 
-  return (request: express.Request, response: express.Response, next: express.NextFunction): void => {
-    try {
-      process(service, request.body, Transport, Protocol, request).then((result: any) => {
-        response.status(200).end(result)
-      }, (err: any) => {
-        next(err)
-      })
-    } catch (err) {
-      next(err)
+    return (request: express.Request, response: express.Response, next: express.NextFunction): void => {
+        try {
+            process(service, request.body, Transport, Protocol, request).then((result: any) => {
+                response.status(200).end(result)
+            }, (err: any) => {
+                next(err)
+            })
+        } catch (err) {
+            next(err)
+        }
     }
-  }
 }
--- packages/thrift-server-express/src/tests/server.ts
+++ packages/thrift-server-express/src/tests/server.ts
@@ -3,58 +3,58 @@ import * as express from 'express'
 import { thriftExpress } from '../main'
 
 import {
-  SharedStruct,
-  SharedUnion,
+    SharedStruct,
+    SharedUnion,
 } from './generated/shared/shared'
 
 import {
-  Calculator,
-  Operation,
-  Work,
+    Calculator,
+    Operation,
+    Work,
 } from './generated/calculator/calculator'
 
 const serviceHandlers: Calculator.IHandler<express.Request> = {
-  ping(): void {
-    return
-  },
-  add(a: number, b: number): number {
-    return a + b
-  },
-  addWithContext(a: number, b: number, context?: express.Request): number {
-    if (context !== undefined && context.headers['x-fake-token'] === 'fake-token') {
-      return a + b
-    } else {
-      throw new Error('Unauthorized')
-    }
-  },
-  calculate(logId: number, work: Work): number {
-    switch (work.op) {
-      case Operation.ADD:
-        return work.num1 + work.num2
-      case Operation.SUBTRACT:
-        return work.num1 - work.num2
-      case Operation.DIVIDE:
-        return work.num1 / work.num2
-      case Operation.MULTIPLY:
-        return work.num1 * work.num2
-    }
-  },
-  zip(): void {
-    return
-  },
-  getStruct(): SharedStruct {
-    return new SharedStruct({
-      key: 0,
-      value: 'test',
-    })
-  },
-  getUnion(index: number): SharedUnion {
-    if (index === 1) {
-      return SharedUnion.fromOption1('foo')
-    } else {
-      return SharedUnion.fromOption2('bar')
-    }
-  },
+    ping(): void {
+        return
+    },
+    add(a: number, b: number): number {
+        return a + b
+    },
+    addWithContext(a: number, b: number, context?: express.Request): number {
+        if (context !== undefined && context.headers['x-fake-token'] === 'fake-token') {
+            return a + b
+        } else {
+            throw new Error('Unauthorized')
+        }
+    },
+    calculate(logId: number, work: Work): number {
+        switch (work.op) {
+            case Operation.ADD:
+                return work.num1 + work.num2
+            case Operation.SUBTRACT:
+                return work.num1 - work.num2
+            case Operation.DIVIDE:
+                return work.num1 / work.num2
+            case Operation.MULTIPLY:
+                return work.num1 * work.num2
+        }
+    },
+    zip(): void {
+        return
+    },
+    getStruct(): SharedStruct {
+        return new SharedStruct({
+            key: 0,
+            value: 'test',
+        })
+    },
+    getUnion(index: number): SharedUnion {
+        if (index === 1) {
+            return SharedUnion.fromOption1('foo')
+        } else {
+            return SharedUnion.fromOption2('bar')
+        }
+    },
 }
 
 const PORT = 8090
@@ -64,7 +64,7 @@ const app = express()
 app.use(
   '/thrift',
   bodyParser.raw(),
-  thriftExpress(Calculator.Processor, serviceHandlers),
+  thriftExpress<Calculator.Processor<express.Request>, Calculator.IHandler>(Calculator.Processor, serviceHandlers),
 )
 
 app.get('/control', (req: express.Request, res: express.Response) => {
