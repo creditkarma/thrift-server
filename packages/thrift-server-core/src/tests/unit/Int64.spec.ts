c1b76b089b4bd199c9a772548698f663802dd016
Kevin Greene <kevin.greene@creditkarma.com>
2019-01-10 15:38:43 -0800

    fix: include Int64 constructor and fix unsafee Buffer usage

--- packages/thrift-server-core/src/tests/unit/Int64.spec.ts
+++ packages/thrift-server-core/src/tests/unit/Int64.spec.ts
@@ -12,30 +12,45 @@ describe('Int64', () => {
     const TEST_STRING: string = '9837756439'
     const TOO_LARGE: string = '999999999999999999999999999999'
 
+    describe('toNumber', () => {
+        it('should return value as number', async () => {
+            const i64 = new Int64(32)
+            expect(i64.toNumber()).to.equal(32)
+        })
+    })
+
     describe('fromDecimalString', () => {
-        it('should correctly create Int64 from string', (done: any) => {
+        it('should correctly create Int64 from string', async () => {
             const i64 = Int64.fromDecimalString(TEST_STRING)
             expect(i64.toDecimalString()).to.equal(TEST_STRING)
-            done()
         })
 
-        it('should throw if the decimal string is too large for Int64', (done: any) => {
+        it('should throw if the decimal string is too large for Int64', async () => {
             expect(() => Int64.fromDecimalString(TOO_LARGE)).to.throw()
-            done()
         })
     })
 
     describe('toDecimalString', () => {
-        it('should correctly create a string representation of number', (done: any) => {
+        it('should correctly create a string representation of number', async () => {
             const i64 = new Int64(54)
             expect(i64.toDecimalString()).to.equal('54')
-            done()
         })
 
-        it('should correctly create a string representation of a hex number', (done: any) => {
+        it('should correctly create a string representation of a hex number', async () => {
             const i64 = new Int64('0xffff')
             expect(i64.toDecimalString()).to.equal('65535')
-            done()
+        })
+    })
+
+    describe('static toDecimalString', () => {
+        it('should correctly create a string representation of number', async () => {
+            const i64 = new Int64(54)
+            expect(Int64.toDecimalString(i64)).to.equal('54')
+        })
+
+        it('should correctly create a string representation of a hex number', async () => {
+            const i64 = new Int64('0xffff')
+            expect(Int64.toDecimalString(i64)).to.equal('65535')
         })
     })
 })
